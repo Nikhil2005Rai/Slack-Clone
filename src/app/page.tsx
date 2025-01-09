201f93ec600aa83b5a68902369cdbeb4ea3def04
Nikhil2005Rai <n2005rai@gmail.com>
2025-01-09 20:33:01 +0530

    user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 "use client";
+import { useEffect, useMemo } from "react";
 
 import UserButton from "@/features/auth/components/user-button";
 
-export default function Home() {
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 
+export default function Home() {
+  const  { data, isLoading } = useGetWorkspaces();
+
+  const workSpaceId = useMemo(() => data?.[0]?._id, [data]);
+
+  useEffect(() => {
+    if(isLoading) return;
+
+    if(workSpaceId){
+      console.log("Redirect to workspace");
+    } else {
+      console.log("Open creation modal")
+    }
+  }, [workSpaceId, isLoading])
+  
   return (
     <div>
       <UserButton />
