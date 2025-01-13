e1c8045c9421aa23cf47bf1b443528e551fb1dfe
Nikhil2005Rai <n2005rai@gmail.com>
2025-01-13 12:46:45 +0530

    add workspace section

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,8 +1,10 @@
-import { AlertTriangle, Loader, MessageSquareText, SendHorizonal } from "lucide-react";
+import { AlertTriangle, HashIcon, Loader, MessageSquareText, SendHorizonal } from "lucide-react";
 
 import SidebarItem from "./sidebar-item";
 import WorkspaceHeader from "./workspace-header";
+import WorkspaceSection from "./workspace-section";
 
+import { useGetChannels } from "@/features/channels/api/use-get-channels";
 import { useCurrentMember } from "@/features/members/api/use-current-members";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id"
@@ -12,18 +14,19 @@ const WorkspaceSidebar = () => {
 
   const { data: member, isLoading: memberLoading } = useCurrentMember({ workspaceId });
   let { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({ id: workspaceId });
+  const { data: channels, isLoading: channelsLoading } = useGetChannels({ workspaceId });
 
-  if(workspaceLoading || memberLoading) {
+  if (workspaceLoading || memberLoading) {
     return (
       <div className="flex flex-col bg-[#5e2c5f] h-full items-center justify-center">
-        <Loader className="size-5 animate-spin text-white"/>
+        <Loader className="size-5 animate-spin text-white" />
       </div>
     )
   }
-  if(!workspace || !member) {
+  if (!workspace || !member) {
     return (
       <div className="flex flex-col gap-y-2 bg-[#5e2c5f] h-full items-center justify-center">
-        <AlertTriangle className="size-5 text-white"/>
+        <AlertTriangle className="size-5 text-white" />
         <p className="text-white text-sm">Workspace not found</p>
       </div>
     )
@@ -31,7 +34,7 @@ const WorkspaceSidebar = () => {
 
   return (
     <div className="flex flex-col bg-[#5e2c5f] h-full">
-      <WorkspaceHeader workspace={workspace} isAdmin={member.role === "admin"}/>
+      <WorkspaceHeader workspace={workspace} isAdmin={member.role === "admin"} />
       <div className="flex flex-col px-2 mt-3">
         <SidebarItem
           label="Threads"
@@ -44,6 +47,20 @@ const WorkspaceSidebar = () => {
           id="drafts"
         />
       </div>
+        <WorkspaceSection 
+         label="Channels"
+         hint="New channel"
+         onNew={() => {}}
+        >
+          {channels?.map((item) => (
+            <SidebarItem
+              key={item._id}
+              icon={HashIcon}
+              label={item.name}
+              id={item._id}
+            />
+          ))}
+        </WorkspaceSection>
     </div>
   )
 }
