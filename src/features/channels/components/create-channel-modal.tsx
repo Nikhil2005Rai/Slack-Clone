b54d69c13cc11576f27b52eb552edd1784386128
Nikhil2005Rai <n2005rai@gmail.com>
2025-01-15 09:59:47 +0530

    complete channel creation feature with api integration

--- src/features/channels/components/create-channel-modal.tsx
+++ src/features/channels/components/create-channel-modal.tsx
@@ -11,8 +11,13 @@ import { Input } from "@/components/ui/input";
 
 import { useCreateChannelModal } from "../store/use-create-channel-modal"
 import { Button } from "@/components/ui/button";
+import { useCreateChannel } from "../api/use-create-channel";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
 
 const CreateChannelModel = () => {
+    const workspaceId = useWorkspaceId();
+
+    const { mutate, isPending } = useCreateChannel();
     const [open, setOpen] = useCreateChannelModal();
 
     const [name, setName] = useState("");
@@ -27,16 +32,29 @@ const CreateChannelModel = () => {
         setName(value);
     };
 
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        mutate(
+            { name, workspaceId },
+            {
+                onSuccess: (id) => {
+                    // TODO: Redirect to new channel
+                    handleClose();
+                }
+            }
+        )
+    }
+
     return (
         <Dialog open={open} onOpenChange={handleClose}>
             <DialogContent>
                 <DialogHeader>
                     <DialogTitle>Add a channel</DialogTitle>
                 </DialogHeader>
-                <form className="space-y-4">
+                <form onSubmit={handleSubmit} className="space-y-4">
                     <Input
                         value={name}
-                        disabled={false}
+                        disabled={isPending}
                         onChange={handleChange}
                         required
                         autoFocus
@@ -45,7 +63,7 @@ const CreateChannelModel = () => {
                         placeholder="e.g. plan-budget"
                     />
                     <div className="flex justify-end">
-                        <Button disabled={false}>
+                        <Button disabled={isPending}>
                             Create
                         </Button>
                     </div>
