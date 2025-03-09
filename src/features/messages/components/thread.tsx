b4d3bdd2a4c42dd81d99d33ae07789b302fa0ac9
Nikhil2005Rai <n2005rai@gmail.com>
2025-03-09 10:14:56 +0530

    complete thread pannel

--- src/features/messages/components/thread.tsx
+++ src/features/messages/components/thread.tsx
@@ -1,8 +1,10 @@
-import { XIcon } from "lucide-react";
+import { AlertTriangle, Loader, XIcon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 
 import { Id } from "../../../../convex/_generated/dataModel";
+import { useGetMessage } from "../api/use-get-message";
+import { Message } from "@/components/message";
 
 interface ThreadProps {
     messageId: Id<"messages">;
@@ -10,6 +12,43 @@ interface ThreadProps {
 };
 
 export const Thread = ({ messageId, onClose }: ThreadProps) => {
+    
+
+     const  { data: message, isLoading: loadingMessage } = useGetMessage({ id: messageId });
+
+     if(loadingMessage) {
+        return (
+            <div className="h-full flex flex-col">
+                <div className="h-[49px] flex justify-between items-center px-4 border-b">
+                    <p className="text-lg font-bold">Thread</p>
+                    <Button onClick={onClose} size="iconSm" variant="ghost">
+                        <XIcon className="size-5 storke-[1.5]" />
+                    </Button>
+                </div>
+                <div className="flex flex-col gap-y-2 h-full items-center justify-center">
+                    <Loader className="size-5 animate-spin text-muted-foreground" />
+                </div>
+            </div>
+        );
+     };
+
+    if (!message) {
+        return (
+            <div className="h-full flex flex-col">
+                <div className="h-[49px] flex justify-between items-center px-4 border-b">
+                    <p className="text-lg font-bold">Thread</p>
+                    <Button onClick={onClose} size="iconSm" variant="ghost">
+                        <XIcon className="size-5 storke-[1.5]" />
+                    </Button>
+                </div>
+                <div className="flex flex-col gap-y-2 h-full items-center justify-center">
+                    <AlertTriangle className="size-5 text-muted-foreground" />
+                    <p className="text-sm text-muted-foreground">Message not found</p>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div className="h-full flex flex-col">
             <div className="h-[49px] flex justify-between items-center px-4 border-b">
@@ -18,6 +57,21 @@ export const Thread = ({ messageId, onClose }: ThreadProps) => {
                     <XIcon className="size-5 storke-[1.5]"/>
                 </Button>
             </div>
+            <Message 
+                hideThreadButton
+                memberId={message.memberId}
+                authorImage={message.user.image}
+                authorName={message.user.name}
+                isAuthor={false}
+                body={message.body}
+                image={message.image}
+                createdAt={message._creationTime}
+                updatedAt={message.updatedAt}
+                id={message._id}
+                reactions={message.reactions}
+                isEditing={false}
+                setEditingId={() => {}}
+            />
         </div>
     );
 };
