60cfb825027af57fb17d298f985c9bebca728e68
Nikhil2005Rai <n2005rai@gmail.com>
2025-03-10 11:42:56 +0530

    add user profile pannel

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -9,15 +9,16 @@ import Toolbar from "./toolbar";
 import { usePanel } from "@/hooks/use-panel";
 import { Id } from "../../../../convex/_generated/dataModel";
 import { Thread } from "@/features/messages/components/thread";
+import { Profile } from "@/features/members/components/profile";
 
 interface WorkspaceIdLayoutProps {
     children: React.ReactNode;
 }
 
 const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
-    const { parentMessageId, onClose } = usePanel();
+    const { parentMessageId, profileMemberId, onClose } = usePanel();
     
-    const showPannel = !!parentMessageId;
+    const showPannel = !!parentMessageId || !!profileMemberId;
 
     return (
         <div className="h-full">
@@ -46,6 +47,11 @@ const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
                                 {parentMessageId ? (
                                     <Thread
                                         messageId={parentMessageId as Id<"messages">}
+                                        onClose={onClose}
+                                    />
+                                ) : profileMemberId ? (
+                                    <Profile 
+                                        memberId={profileMemberId as Id<"members">}
                                         onClose={onClose}
                                     />
                                 ) : (
