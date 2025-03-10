139fd426a8d93a4af4726fa6d4b810b7baa031fa
Nikhil2005Rai <n2005rai@gmail.com>
2025-03-10 13:34:02 +0530

    fix emoji picker

--- src/components/emoji-popover.tsx
+++ src/components/emoji-popover.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import data from "@emoji-mart/data"
 import Picker from "@emoji-mart/react"
+import EmojiPicker, { EmojiClickData } from "emoji-picker-react";
 
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import {
@@ -14,7 +15,7 @@ import {
 interface EmojiPopoverProps {
     children: React.ReactNode;
     hint?: string;
-    onEmojiSelect: (emoji: string) => void;
+    onEmojiSelect: (value: string) => void;
 }
 
 export const EmojiPopover = ({
@@ -25,8 +26,8 @@ export const EmojiPopover = ({
     const [popoverOpen, setPopoverOpen] = useState(false);
     const [tooltipOpen, setTooltipOpen] = useState(false);
 
-    const onSelect = (emoji: any) => {
-        onEmojiSelect(emoji);
+    const onSelect = (value: EmojiClickData) => {
+        onEmojiSelect(value.emoji);
         setPopoverOpen(false);
 
         setTimeout(() => {
@@ -50,7 +51,7 @@ export const EmojiPopover = ({
                     </TooltipContent>
                 </Tooltip>
                 <PopoverContent className="p-0 w-full border-none shadow-none">
-                    <Picker data={data} onEmojiSelect={onSelect} />
+                    <EmojiPicker onEmojiClick={onSelect} />
                 </PopoverContent>
             </Popover>
         </TooltipProvider>
